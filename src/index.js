0832da89161afed382f0e0fa1aeb466270237802
jennlai95 <jenniferlaai@gmail.com>
2023-12-13 17:29:16 +1100

    feat: heroku procfile now exist

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { app } = require('./server');
 
 const { databaseConnect } = require("./database");
 
+const PORT = process.env.PORT || 3001;  
+
 app.listen(PORT, async () => {
     await databaseConnect();
     console.log("Server running!");
