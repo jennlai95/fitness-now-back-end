c47a5fa0fbce8c3ad352344a2939c72024ba0c42
jennlai95 <jenniferlaai@gmail.com>
2023-12-14 16:17:04 +1100

    create booking and schedule controller

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -1,60 +1,62 @@
 // import Express library and create instance of a router
 const express = require('express');
-const { Class } = require('../models/ClassModel');
+const { Booking } = require('../models/BookingModel');
 const router = express.Router();
 
 
 
-// class needs to be an admin for access
 
 
-// CRUD: create read update and deletre 
+// CRUD: create read update and delete 
 
 
 // READ
-// Find ALL Class in the DB
+// Find ALL bookings in the DB  
+// GET /bookings
+// needs to be admin 
 router.get("/all", async (request, response) => {
 	// Empty object in .find() means get ALL documents
-	let result = await Class.find({});
+	let result = await Booking.find({});
 
 	response.json({
-		classes: result
+		result
 	});
 
 });
 
 //READ
-// Find one user by its ID
-router.get("/:id", async (request, response) => {
+// Find one booking  by its ID
+router.get("id/:id", async (request, response) => {
 	let result = null;
 
 	response.json({
-		class: result
+		 result
 	});
 
 });
 
-// CREATE a new Class  in the DB
+// update and delete needs to be an admin for access
+// CREATE a new booking  in the DB
 // POST localhost:3000/users/
 router.post("/", async (request, response) => {
 
 	// Error handling via Promise.catch()
-	let result = await Class.create(request.body).catch(error => {return error});
+	let result = await Booking.create(request.body).catch(error => {return error});
 	
 
 	response.json({
-		class: result
+		 result
 	});
 
 });
 
-// UPDATE an existing class in DB 
+// UPDATE an existing schedule in DB 
 // patch modidies whatever properties is provided and doesn't overwrite or remove any unmentioned properties
 router.patch("/:id", async (request, response) => {
 	let result = null;
 
 	response.json({
-	    class : result
+	     result
 	});
 
 });
@@ -65,7 +67,7 @@ router.delete("/:id", async (request, response) => {
 	let result = null;
 
 	response.json({
-		class: result
+	   result
 	});
 
 });
--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -1,60 +1,60 @@
 // import Express library and create instance of a router
 const express = require('express');
-const { Class } = require('../models/ClassModel');
+const { Schedule } = require('../models/ScheduleModel');
 const router = express.Router();
 
 
 
-// class needs to be an admin for access
 
 
-// CRUD: create read update and deletre 
+// CRUD: create read update and delete 
 
 
 // READ
-// Find ALL Class in the DB
+// Find ALL schedule in the DB
 router.get("/all", async (request, response) => {
 	// Empty object in .find() means get ALL documents
-	let result = await Class.find({});
+	let result = await Schedule.find({});
 
 	response.json({
-		classes: result
+		result
 	});
 
 });
 
 //READ
-// Find one user by its ID
+// Find one schedule by its ID
 router.get("/:id", async (request, response) => {
 	let result = null;
 
 	response.json({
-		class: result
+		 result
 	});
 
 });
 
-// CREATE a new Class  in the DB
+// read, update and delete needs to be an admin for access
+// CREATE a new Schedule  in the DB
 // POST localhost:3000/users/
 router.post("/", async (request, response) => {
 
 	// Error handling via Promise.catch()
-	let result = await Class.create(request.body).catch(error => {return error});
+	let result = await Schedule.create(request.body).catch(error => {return error});
 	
 
 	response.json({
-		class: result
+		 result
 	});
 
 });
 
-// UPDATE an existing class in DB 
+// UPDATE an existing schedule in DB 
 // patch modidies whatever properties is provided and doesn't overwrite or remove any unmentioned properties
 router.patch("/:id", async (request, response) => {
 	let result = null;
 
 	response.json({
-	    class : result
+	     result
 	});
 
 });
@@ -65,7 +65,7 @@ router.delete("/:id", async (request, response) => {
 	let result = null;
 
 	response.json({
-		class: result
+	   result
 	});
 
 });
