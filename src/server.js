8a4762e87f96faa42e03ab381c091314b7fa166b
jennlai95 <126547898+jennlai95@users.noreply.github.com>
2023-12-23 18:59:11 +0800

    update cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,6 @@
 
 const express = require('express');
 
-// make a server instance 
-const app = express();
-
-app.use(express.json());
-
 // CORS server instances;
 
 const cors = require("cors");
@@ -15,13 +10,18 @@ var corsOptions = {
   origin: [
     "http://localhost:3001",
     "http://localhost:3000",
-	"https://t3a2-fitness-now.netlify.app/"
+	"https://t3a2-fitness-now.netlify.app"
   ],
 
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
+
+// make a server instance 
+const app = express();
+
+app.use(express.json());
+
 
 
 // API response 
